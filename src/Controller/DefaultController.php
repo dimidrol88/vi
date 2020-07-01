52f3fa18cbea36765cf2ae5b4217829714b81219
dimidrol88 <kuznecov_da@inbox.ru>
2020-07-01 12:55:00 +0300

    Добавлен скелет symfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'version' => $this->getParameter('app.version'),
+        ]);
+    }
+}
