547a1d82da1950a577d07da8bc5d1583f83d34a5
dimidrol88 <kuznecov_da@inbox.ru>
2020-07-02 17:01:17 +0300

    Добавлено использование аннотаций для маршрутов

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,10 +6,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
+     * @Route("", name="", methods={"GET"})
      * @return Response
      */
     public function index(): Response
